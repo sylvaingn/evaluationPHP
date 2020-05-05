2324ac293950167d0f1fae39eae9f0bfc74cbbab
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-05-05 16:26:34 +0200

    mise en page

--- traitementAjout.php
+++ traitementAjout.php
@@ -1,10 +1,10 @@
 <?php
 
-var_dump($_POST);
-var_dump($_FILES);
-var_dump($_FILES["photo"]);
+//var_dump($_POST);
+//var_dump($_FILES);
+//var_dump($_FILES["photo"]);
 
-var_dump(pathinfo($_FILES["photo"]["name"]));
+//var_dump(pathinfo($_FILES["photo"]["name"]));
 
 /* Si l'extension n'est pas bonne  */
 /* Si le format n'est pas bon */
@@ -60,40 +60,68 @@ function sentFile($nomFichier)
 }
 
 ?>
+<!doctype html>
+<html lang="en">
 
-<?php if (
-    empty($_POST["titre"])
-    or empty($_POST["adresse"])
-    or empty($_POST["ville"])
-    or empty($_POST["cp"])
-    or empty($_FILES["photo"]["name"])
-    or empty($_POST["surface"])
-    or empty($_POST["prix"])
-    or empty($_POST["type"])
-    or empty($_POST["description"])
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-) : ?>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
 
-    <h2>Un des champs est manquant.</h2>
-    <a href="#">Retour en arrière</a>
+<body>
 
-<?php endif; ?>
+    <?php include("fix/navbar.php") ?>
 
-<?php if (checkfile($_FILES["photo"]) == false) : ?>
+    <?php if (
+        empty($_POST["titre"])
+        or empty($_POST["adresse"])
+        or empty($_POST["ville"])
+        or empty($_POST["cp"])
+        or empty($_FILES["photo"]["name"])
+        or empty($_POST["surface"])
+        or empty($_POST["prix"])
+        or empty($_POST["type"])
+        
 
-<?php $nom = saveFile($_FILES["photo"]);
-            sentFile($nom)
-    ?>
+    ) : ?>
+
+        <h2>Un des champs est manquant.</h2>
+        <a href="addLogement.php">Retour en arrière</a>
+
+    <?php endif; ?>
+
+    <?php if (checkfile($_FILES["photo"]) == false) : ?>
+
+        <?php $nom = saveFile($_FILES["photo"]);
+        sentFile($nom)
+        ?>
 
 
-    <h2>Bravo, votre annonce est publiée !</h2>
-    <br>
+        <h2>Bravo, votre annonce est publiée !</h2>
+        <br>
 
 
-    <a href="index.php" class="btn btn-secondary">Retour à la page d'accueil !</a>
+        <a href="index.php" class="btn btn-secondary">Retour à la page d'accueil !</a>
 
-<?php else : ?>
-    <h2>Le format de la photo n'est pas au format jpeg</h2>
+    <?php else : ?>
+        <h2>Le format de la photo n'est pas au format jpeg</h2>
 
 
-<?php endif; ?>
+    <?php endif; ?>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
